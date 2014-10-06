877e7d30467f815e458461cc8255de194e4b544e
Jisse Reitsma <info@yireo.com>
2014-10-06 16:12:08 +0200

    Pending changes

---
+++ source/app/code/community/Yireo/EmailTester/Block/Form/Check.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Yireo EmailTester for Magento 
+ *
+ * @package     Yireo_EmailTester
+ * @author      Yireo (http://www.yireo.com/)
+ * @copyright   Copyright (C) 2014 Yireo (http://www.yireo.com/)
+ * @license     Open Source License
+ * @contributor Philipp Wiegel
+ */
+
+class Yireo_EmailTester_Block_Form_Check extends Mage_Adminhtml_Block_Widget_Container
+{
+    /*
+     * Constructor method
+     */
+    public function _construct()
+    {
+        parent::_construct();
+        $this->setTemplate('emailtester/form/checks.phtml');
+    }
+    
+    public function getWarnings()
+    {
+        $warnings = array();
+
+        return $warnings;
+    }
+}
